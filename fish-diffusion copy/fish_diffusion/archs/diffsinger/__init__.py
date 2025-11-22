a503691c2bb7cecd742b39270b568e6da1f99a88
Gannonnn <writetogannon@gmail.com>
2025-11-22 17:44:28 -0600

    second

--- fish-diffusion copy/fish_diffusion/archs/diffsinger/__init__.py
+++
@@ -1,4 +0,0 @@
-from .diffsinger import DiffSinger, DiffSingerLightning
-from .grad_tts import GradTTS
-
-__all__ = ["DiffSingerLightning", "DiffSinger", "GradTTS"]
--- fish-diffusion copy/fish_diffusion/modules/vocoders/__init__.py
+++
@@ -1,7 +0,0 @@
-from .adamos import ADaMoSHiFiGANV1
-from .builder import VOCODERS
-from .istft_net.istft_net import ISTFTNet
-from .nsf_hifigan import NsfHifiGAN
-from .refinegan import RefineGAN
-
-__all__ = ["VOCODERS", "NsfHifiGAN", "ISTFTNet", "RefineGAN", "ADaMoSHiFiGANV1"]
