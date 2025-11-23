6cc5101bee0ade5d1d0e1e50377ca8bf420c7d73
Marko Kupresanin <kupresaninmarko2@gmail.com>
2025-11-22 18:19:12 -0600

    frontend boilerplate

---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Replace 'username' and 'repo' with your GitHub info
+const repoName = 'MadHacksReal'
+
+export default defineConfig({
+  plugins: [react()],
+  base: `/${repoName}/`, // required for GitHub Pages
+  build: {
+    outDir: '../docs',  // build goes to root/docs
+    emptyOutDir: true,  // clears old build files
+  },
+})
+
